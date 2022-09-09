edecdfdfd1c63a9e6e582b51082fd6e28b9777f8
Konstantin <kosvadmail@atomlc.com.ua>
2022-09-09 07:09:20 +0200

    Исходные сайты

--- gulpfile.js
+++ gulpfile.js
@@ -9,71 +9,7 @@ import svgo from 'gulp-svgmin';
 import del from 'del';
 import browser from 'browser-sync';
 
-// Styles
 
-export const styles = () => gulp.src('source/assets/css/*.css', {
-  sourcemaps: true
-})
-  .pipe(plumber())
-  .pipe(postcss([
-    autoprefixer(),
-    csso()
-  ]))
-  .pipe(gulp.dest('build/assets/css', {
-    sourcemaps: '.'
-  }))
-  .pipe(browser.stream());
-
-// HTML
-
-const html = () => gulp.src('source/*.html')
-  .pipe(htmlmin({
-    collapseWhitespace: true
-  }))
-  .pipe(gulp.dest('build'));
-
-// Scripts
-
-const scripts = () => gulp.src('source/assets/js/**/*.js')
-  .pipe(gulp.dest('build/assets/js'))
-  .pipe(browser.stream());
-
-// PHP
-const php = () => gulp.src('source/assets/php/**/*.php')
-  .pipe(gulp.dest('build/assets/php'))
-  .pipe(browser.stream());
-
-// Images
-
-const optimizeImages = () => gulp.src('source/assets/img/**/*.{png,jpg,jpeg}')
-  .pipe(imagemin())
-  .pipe(gulp.dest('build/assets/img'));
-const copyIco = () => gulp.src('source/assets/img/**/*.{ico}')
-  .pipe(gulp.dest('build/assets/img/favicons'));
-
-const copyImages = () => gulp.src('source/assets/img/**/*.{png,jpg,jpeg,ico}')
-  .pipe(gulp.dest('build/assets/img'));
-
-
-// Video
-
-const optimizeVideos = () => gulp.src('source/assets/video/**/*.{mp4}')
-  .pipe(gulp.dest('build/assets/video'));
-
-const copyVideos = () => gulp.src('source/assets/video/**/*.{mp4}')
-  .pipe(gulp.dest('build/assets/video'));
-
-// SVG
-
-const svg = () =>
-  gulp.src('source/assets/img/**/*.svg')
-    .pipe(svgo())
-    .pipe(gulp.dest('build/assets/img'));
-
-
-// Clean
-
-const clean = () => del('build');
 
 // Server
 
@@ -99,43 +35,15 @@ const reload = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch('source/assets/css/**/*.css', gulp.series(styles));
-  gulp.watch('source/assets/js/**/*.js', gulp.series(scripts));
+  gulp.watch('source/assets/css/**/*.css', gulp.series(styles, reload));
+  gulp.watch('source/assets/js/**/*.js', gulp.series(scripts, reload));
   gulp.watch('source/*.html', gulp.series(html, reload));
 };
 
-// Build
-
-export const build = gulp.series(
-  clean,
-  copyIco,
-  optimizeImages,
-  optimizeVideos,
-  gulp.parallel(
-    styles,
-    html,
-    php,
-    scripts,
-    svg
-  ),
-  gulp.series(
-    server
-  ));
 
 // Default
 
 export default gulp.series(
-  clean,
-  copyImages,
-  copyVideos,
-  gulp.parallel(
-    styles,
-    html,
-    php,
-    scripts,
-    svg
-  ),
   gulp.series(
-    server,
-    watcher
+    server
   ));
